e8c923675720d53057547c41633a8c7e43c30b34
Philip Double <doublep@ctc.com>
2014-01-06 17:29:45 -0500

    Placing the medialment style pieces in the correct place and placing traces to find lock issues...

--- bin/core-files/js/C_Engine.js
+++ bin/core-files/js/C_Engine.js
@@ -188,8 +188,9 @@ function openLockRequest(_data){
 
 function openLockRequestAccepted(_data){
 	//console.log("requested Beatch!?!?");
+	console.log(_data);
 	if(username == _data.requester){
-		var msg = '<div id="dialog-incomingLockRequest" title="Request for Edit Control"><p class="validateTips">'+ _data.requestee +' has passed you the lock to edit this lesson.</p><p>You currently hold the lock on edit controls. <b>Be certain to close this lesson window or relinquish lock if you are not actively working on the lesson!</b></p></div>';
+		var msg = '<div id="dialog-incomingLockRequest" title="Request for Edit Control"><p class="validateTips">'+ _data.me +' has passed you the lock to edit this lesson.</p><p>You currently hold the lock on edit controls. <b>Be certain to close this lesson window or relinquish lock if you are not actively working on the lesson!</b></p></div>';
 			
 		//Add to stage.
 		$("#stage").append(msg);
@@ -216,7 +217,7 @@ function openLockRequestAccepted(_data){
 
 function openLockRequestRefused(_data){
 	if(username == _data.requester){
-		var msg = '<div id="dialog-incomingLockRequest" title="Request for Edit Control"><p class="validateTips">'+ _data.requestee +' has refused your request for edit controls.  Contact them at their e-mail to follow up and plan access.</p></div>';
+		var msg = '<div id="dialog-incomingLockRequest" title="Request for Edit Control"><p class="validateTips">'+ _data.me +' has refused your request for edit controls.  Contact them at their e-mail to follow up and plan access.</p></div>';
 			
 		//Add to stage.
 		$("#stage").append(msg);
