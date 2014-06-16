bfb14ccf701bf22858b18d8acec83b6663bb3db5
shumaket <shumaket@ctc.com>
2014-06-16 16:00:56 -0400

    added check for Browser modes 5, 6 and 7 (quirks mode)

--- bin/core-files/js/C_Engine.js
+++ bin/core-files/js/C_Engine.js
@@ -61,77 +61,152 @@ $(document).ready(function(){
 function initScripts(_data){
 	data = _data;
 	//console.log(data);
-	totalPages = $(data).find('page').length;
-	mode = $(data).find('mode').attr("value");
-	if(mode == "production" || mode == "Production" || mode == "prod"){
-		corePath = "";
+
+	// Create new ieUserAgent object
+	var ieUserAgent = {
+	    init: function () {
+	        // Get the user agent string
+	        var ua = navigator.userAgent;
+	        this.compatibilityMode = false;
+	        
+	        // Detect whether or not the browser is IE
+	        var ieRegex = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+	        if (ieRegex.exec(ua) == null)
+	            this.exception = "The user agent detected does not contain Internet Explorer.";
+	 
+	        // Get the current "emulated" version of IE
+	        this.renderVersion = parseFloat(RegExp.$1);
+	        this.version = this.renderVersion;
+	 
+	        // Check the browser version with the rest of the agent string to detect compatibility mode
+	        if(document.documentMode == 5 || document.documentMode == 6 || document.documentMode == 7){
+	        	this.compatibilityMode = true;
+	        	this.version = document.documentMode;
+	        }
+	        else if (ua.indexOf("Trident/6.0") > -1) {
+	            if (ua.indexOf("MSIE 7.0") > -1) {
+	                this.compatibilityMode = true;
+	                this.version = 10;                  // IE 10
+	            }
+	        }
+	        else if (ua.indexOf("Trident/5.0") > -1) {      
+	            if (ua.indexOf("MSIE 7.0") > -1) {
+	                this.compatibilityMode = true;
+	                this.version = 9;                   // IE 9
+	            }
+	        }
+	        else if (ua.indexOf("Trident/4.0") > -1) {
+	            if (ua.indexOf("MSIE 7.0") > -1) {
+	                this.compatibilityMode = true;
+	                this.version = 8;                   // IE 8
+	            }
+	        }
+	        else if (ua.indexOf("MSIE 7.0") > -1){
+	            this.version = 7;                       // IE 7
+	            this.compatibilityMode = true;     //not truly compatibilityMode IE7 is not supported
+	        }
+	        
+	        else{
+	        	this.compatibilityMode = false;
+	            this.version = -1;                       // IE 6	        	
+	        }
+
+	    }
+	};
+	 
+	// Initialize the ieUserAgent object
+	ieUserAgent.init();
+
+    var val = "IE" + ieUserAgent.version;
+    if (ieUserAgent.compatibilityMode)
+    {
+        val += " Compatibility Mode (IE" + ieUserAgent.renderVersion + " emulation)";
+        //alert(val);
+    	$('body').empty();
+		$('body').append("<div style=\"background-color: #FFFFFF\"> <p>We have detected the following IE browser: " + val + "<br/><br/>"+
+			"This content has been tested on IE8+, Chrome, Firefox and Safari but does not support Compatibility Mode or Document Mode of \"Quirks Mode\" and \"IE7 Standards\" in IE.<br/></p>"+
+			"<p>Instructions for disabling Compatibility Mode:</p>"+
+			"<ol><li>Go to tools (on the top) on Internet Explorer 8, 9 or 10.</li>"+
+			"<li>Click on \"Compatibility View Settings\".</li>"+
+			"<li>Uncheck \"Include updated websites list from Microsoft\".</li>"+
+			"<li>Uncheck \"Display all websites in compatibility view\" if it is checked.</li>"+
+			"<li>Click on \"Close\". You are now done. Please refresh the page.</li></ol></div>");
+    }
+    else{
+
+		totalPages = $(data).find('page').length;
+		mode = $(data).find('mode').attr("value");
+		if(mode == "production" || mode == "Production" || mode == "prod"){
+			corePath = "";
+		}
+
+	    // This will prevent errors on slow connections.  We might need to set it to an actual number, as 0 means no timeout.
+	    require.config({
+	        waitSeconds: 0
+	    });
+		//GATHERING AND LOADING ALL OF THE ENGINE PARTS
+		require([		
+			//Funtionality/utilities
+					corePath +"js/libs/jqueryui/jquery-ui.min.js",
+					corePath +"js/libs/SCORM_API_wrapper.js", //SCORM capabilities
+					corePath +"js/libs/jquery.ui.touch-punch.min.js", //Adds touch drag to touchscreen devices.
+					corePath +"js/libs/antiscroll.js",
+					corePath +"js/libs/overthrow.min.js",
+					corePath +"js/libs/socket-client/socket.io.min.js", //required for edit mode.
+					corePath +"js/libs/ckeditor/ckeditor.js",
+					corePath +"js/libs/ckeditor/adapters/jquery.js",
+					corePath +"js/libs/C_DynamicBackgroundImage.js", //Allows us to set an image background on all browsers
+					corePath +"js/libs/mediaElement/mediaelement-and-player.js", //Our audio and video solution
+					corePath +"js/libs/greensock/TweenMax.min.js", //Our animation library.
+					corePath +"js/libs/jquery.swfobject.1-1-1.min.js", //Method to embed .swf files.
+					corePath +"js/libs/jquery.nestable.js",
+					corePath +"js/libs/jquery.browser.js",
+					corePath +"js/libs/jquery.corner.js",
+					corePath +"js/libs/modernizr.js",
+					corePath +"js/libs/siofu/client.js",
+					corePath +"js/libs/pretty-data.js",
+			//Import Cognizen layout templates
+					//corePath +"js/templates/C_LessonTitle.js",
+					corePath +"js/templates/C_Categories.js",
+					corePath +"js/templates/C_ClickImage.js",
+					corePath +"js/templates/C_Completion.js", 
+					corePath +"js/templates/C_StaticContent.js", //All text and static media pages - text, .jpg, .png, .swf
+					corePath +"js/templates/C_TabbedContent.js", //Tabs can be added to static by power users but this is more user friendly.
+					corePath +"js/templates/C_Reveal.js", //Reveal text upon clicking on an image.
+					corePath +"js/templates/C_Flashcard.js",
+					corePath +"js/templates/C_Unity3D.js", //Template for importing Unity 3D swf files - requires more than regular swf
+			//Import Cognizen Knowledge Check templates
+					corePath +"js/templates/C_MultipleChoice.js", //Multiple choice quizzing
+					corePath +"js/templates/C_MultipleChoiceImage.js",
+					corePath +"js/templates/C_Matching.js", //Matching quizzing
+					corePath +"js/templates/C_QuestionBank.js", //Matching quizzing
+					corePath +"js/templates/C_Sequencing.js",
+					corePath +"js/templates/C_TextInput.js",
+			//Import Cognizen Components
+					corePath +"js/components/C_PageTitle.js",
+					corePath +"js/components/C_VisualMediaHolder.js",
+					corePath +"js/components/C_AudioHolder.js",
+			//Import Cognizen Utilities
+					corePath +"js/util/C_AddPage.js",
+					corePath +"js/util/C_API.js",
+					corePath +"js/util/C_Comment.js",
+					corePath +"js/util/C_DocList.js",
+					corePath +"js/util/C_EditorToolbarFormat.js",
+					corePath +"js/util/C_Glossary.js",
+					corePath +"js/util/C_Index.js",
+					corePath +"js/util/C_Lock.js",
+					corePath +"js/util/C_NavControl.js",
+					corePath +"js/util/C_ScrubContent.js",
+					corePath +"js/util/C_SCORM.js",
+					corePath +"js/util/C_Sockets.js",
+					corePath +"js/util/C_UtilFunctions.js",
+			//Give mouse super powers.
+					corePath +"js/libs/jquery.mousewheel-3.0.6.pack.js",
+			//Lightbox for media popups and galleries.
+					corePath +"js/libs/fancybox/jquery.fancybox.js",
+					corePath +"js/libs/fancybox/jquery.fancybox-thumbs.js"
+		], startEngine);
 	}
-    // This will prevent errors on slow connections.  We might need to set it to an actual number, as 0 means no timeout.
-    require.config({
-        waitSeconds: 0
-    });
-	//GATHERING AND LOADING ALL OF THE ENGINE PARTS
-	require([		
-		//Funtionality/utilities
-				corePath +"js/libs/jqueryui/jquery-ui.min.js",
-				corePath +"js/libs/SCORM_API_wrapper.js", //SCORM capabilities
-				corePath +"js/libs/jquery.ui.touch-punch.min.js", //Adds touch drag to touchscreen devices.
-				corePath +"js/libs/antiscroll.js",
-				corePath +"js/libs/overthrow.min.js",
-				corePath +"js/libs/socket-client/socket.io.min.js", //required for edit mode.
-				corePath +"js/libs/ckeditor/ckeditor.js",
-				corePath +"js/libs/ckeditor/adapters/jquery.js",
-				corePath +"js/libs/C_DynamicBackgroundImage.js", //Allows us to set an image background on all browsers
-				corePath +"js/libs/mediaElement/mediaelement-and-player.js", //Our audio and video solution
-				corePath +"js/libs/greensock/TweenMax.min.js", //Our animation library.
-				corePath +"js/libs/jquery.swfobject.1-1-1.min.js", //Method to embed .swf files.
-				corePath +"js/libs/jquery.nestable.js",
-				corePath +"js/libs/jquery.browser.js",
-				corePath +"js/libs/jquery.corner.js",
-				corePath +"js/libs/modernizr.js",
-				corePath +"js/libs/siofu/client.js",
-				corePath +"js/libs/pretty-data.js",
-		//Import Cognizen layout templates
-				//corePath +"js/templates/C_LessonTitle.js",
-				corePath +"js/templates/C_Categories.js",
-				corePath +"js/templates/C_ClickImage.js",
-				corePath +"js/templates/C_Completion.js", 
-				corePath +"js/templates/C_StaticContent.js", //All text and static media pages - text, .jpg, .png, .swf
-				corePath +"js/templates/C_TabbedContent.js", //Tabs can be added to static by power users but this is more user friendly.
-				corePath +"js/templates/C_Reveal.js", //Reveal text upon clicking on an image.
-				corePath +"js/templates/C_Flashcard.js",
-				corePath +"js/templates/C_Unity3D.js", //Template for importing Unity 3D swf files - requires more than regular swf
-		//Import Cognizen Knowledge Check templates
-				corePath +"js/templates/C_MultipleChoice.js", //Multiple choice quizzing
-				corePath +"js/templates/C_MultipleChoiceImage.js",
-				corePath +"js/templates/C_Matching.js", //Matching quizzing
-				corePath +"js/templates/C_QuestionBank.js", //Matching quizzing
-				corePath +"js/templates/C_Sequencing.js",
-				corePath +"js/templates/C_TextInput.js",
-		//Import Cognizen Components
-				corePath +"js/components/C_PageTitle.js",
-				corePath +"js/components/C_VisualMediaHolder.js",
-				corePath +"js/components/C_AudioHolder.js",
-		//Import Cognizen Utilities
-				corePath +"js/util/C_AddPage.js",
-				corePath +"js/util/C_API.js",
-				corePath +"js/util/C_Comment.js",
-				corePath +"js/util/C_DocList.js",
-				corePath +"js/util/C_EditorToolbarFormat.js",
-				corePath +"js/util/C_Glossary.js",
-				corePath +"js/util/C_Index.js",
-				corePath +"js/util/C_Lock.js",
-				corePath +"js/util/C_NavControl.js",
-				corePath +"js/util/C_ScrubContent.js",
-				corePath +"js/util/C_SCORM.js",
-				corePath +"js/util/C_Sockets.js",
-				corePath +"js/util/C_UtilFunctions.js",
-		//Give mouse super powers.
-				corePath +"js/libs/jquery.mousewheel-3.0.6.pack.js",
-		//Lightbox for media popups and galleries.
-				corePath +"js/libs/fancybox/jquery.fancybox.js",
-				corePath +"js/libs/fancybox/jquery.fancybox-thumbs.js"
-	], startEngine);
 }
 
 //VROOM VROOM
