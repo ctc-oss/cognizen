c2b3a7f7dc1a6e166b11d6e27fb59ce075bcf353
Philip Double <doublep@ctc.com>
2015-01-26 08:47:16 -0500

    Addind slider to tab index_arr

--- bin/core-files/js/templates/C_Slider.js
+++ bin/core-files/js/templates/C_Slider.js
@@ -129,7 +129,7 @@ function C_Slider(_type) {
 		});
 		
 		pageAccess_arr.push($("#display"))
-		//pageAccess_arr.push($("#slider"));
+		pageAccess_arr.push($("#slider"));
 		
 		if(!isComplete){
 			$("#contentHolder").append('<div id="mcSubmit"></div>');
