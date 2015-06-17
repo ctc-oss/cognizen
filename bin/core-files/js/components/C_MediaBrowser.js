c2eb1cfb255c9355b143e58b0f189294780e6b51
Philip Double <doublep@ctc.com>
2015-06-17 15:18:15 -0400

    Reverting - test was negative.

--- bin/core-files/js/components/C_MediaBrowser.js
+++ bin/core-files/js/components/C_MediaBrowser.js
@@ -633,7 +633,7 @@ function mediaBrowserRemoveMediaComplete(){
 * @param {String} _file File to be loaded and displayed.
 */
 function mediaBrowserPreviewFile(_file){
-	$("#mediaBrowserPreview").addClass("mediaBrowserLoader");
+	$("#mediaBrowserPreview").addClass("C_Loader");
 	var imageTypes = ["png", "jpg", "gif"];
 	var fp = folderTrackPath + mediaBrowserDisplayPath + _file;
 	$("#mediaBrowserPreviewMediaHolder").empty();
@@ -648,7 +648,7 @@ function mediaBrowserPreviewFile(_file){
 	}else if(imageTypes.indexOf(myType) > -1) {
 		mediaBrowserLoadImagePreview(fp);
 	}else{
-		$("#mediaBrowserPreview").removeClass("mediaBrowserLoader");
+		$("#mediaBrowserPreview").removeClass("C_Loader");
 		//alert("You can't preview this file type.");
 		$("#mediaBrowserPreviewMediaHolder").append("<p><b>You can't preview this file type from within the system.</b></p><p>It has been opened in a new window. Check how your browser handles different file types to see if it has been downloaded.</p>");
 		$("#mediaBrowserPreviewMediaHolder").css({'opacity': 1, 'text-align': 'center'});
@@ -693,7 +693,7 @@ function mediaBrowserLoadAudioPreview(_fp){
             }, false);
             
             player.addEventListener('loadeddata', function(e){
-	            $("#mediaBrowserPreview").removeClass("mediaBrowserLoader");
+	            $("#mediaBrowserPreview").removeClass("C_Loader");
 				TweenMax.to($('#mediaBrowserPreviewMediaHolder'), .5, {css:{opacity:1}, ease:transitionType});
             }, false);
             
@@ -738,7 +738,7 @@ function mediaBrowserLoadVideoPreview(_fp){
 		        }*/
 				
 				//tween after loaded and positioned.
-				$("#mediaBrowserPreview").removeClass("mediaBrowserLoader");
+				$("#mediaBrowserPreview").removeClass("C_Loader");
 				TweenMax.to($('#mediaBrowserPreviewMediaHolder'), .5, {css:{opacity:1}, ease:transitionType});
 			}, false);
 		}
@@ -753,7 +753,7 @@ function mediaBrowserLoadVideoPreview(_fp){
 */
 function mediaBrowserLoadSWFPreview(_fp){
     $("#mediaBrowserPreviewMediaHolder").flash({swf:_fp,width:300,height:200});
-    $("#mediaBrowserPreview").removeClass("mediaBrowserLoader");
+    $("#mediaBrowserPreview").removeClass("C_Loader");
     TweenMax.to($('#mediaBrowserPreviewMediaHolder'), .5, {css:{opacity:1}, ease:transitionType});
 }
 
@@ -771,7 +771,7 @@ function mediaBrowserLoadImagePreview(_fp){
 	});
 
     $(img).load(function(){
-	    $("#mediaBrowserPreview").removeClass("mediaBrowserLoader");
+	    $("#mediaBrowserPreview").removeClass("C_Loader");
         $("#mediaBrowserPreviewMediaHolder").append(img);
         var imageWidth = $(img).width();
         
