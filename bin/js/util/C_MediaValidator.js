b12d801ca5a683dca9b83e62e4725fc5d7d7e793
shumaket <shumaket@ctc.com>
2017-03-03 08:55:58 -0500

    updated mediavalidator to validate templates that were not working.  Fixes #5035

--- bin/js/util/C_MediaValidator.js
+++ bin/js/util/C_MediaValidator.js
@@ -25,6 +25,7 @@ function C_MediaValidator(_myItem, _myParent) {
     var loadedVMModules;									//Variable to track how many module xml files have been loaded.
 	var mediaArr = [];
 	var removedArr = [];
+	var allGood = true;
 
     $(document).ready(function(){
     	initMediaValidator();
@@ -223,7 +224,8 @@ function C_MediaValidator(_myItem, _myParent) {
 		     	msg = '<div id="dialog-mv" title="Media Validator (Beta) : '+ searchTitle + ':">';
 			    msg += '<div id="mvPane" class="pane">'
 			    msg += '<div id="mediaValidatorPane" class="paneContent">';
-			    msg += '<h3>Preferences</h3>';
+			    //msg += '<h3>Preferences</h3>';
+			    msg += '<div id="allGood"></div>';
 			    var helpUrl = $(data).find('help').attr("url");
 			    var icon = validateMedia(helpUrl) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
 			    msg += 'help url : ' + helpUrl + " " + icon;
@@ -231,108 +233,205 @@ function C_MediaValidator(_myItem, _myParent) {
 
 				for(var i = 0; i < totalPages; i++){
 
-				 	msg += '<h3>' + $(data).find("page").eq(i).find('title').first().text() + '</h3>';
+				 	/////#5035///////
+					var branchCount = $(data).find("page").eq(i).children("branch").length;
+					
+					if(branchCount != 0){
+						msg += 'branchCount : ' + branchCount + '<br/>';
+						for (var h = 0; h < branchCount; h++) {	
+							var branchTitle = $(data).find("page").eq(i).children("branch").eq(h).find("title").text().replace(/<\/?[^>]+(>|$)/g, "");
+							var branchType = $(data).find("page").eq(i).children("branch").eq(h).attr('layout');
+							msg += '<br/>////////////////////////////////////////////////////<br/>';
+							msg += branchTitle + ' - ' + branchType + '<br/>';	
+							if(branchType != 'textOnly' && branchType != 'sidebar'){
+								var img = $(data).find("page").eq(i).children("branch").eq(h).attr('img');
+							 	if(typeof img !== typeof undefined && img !== 'null' && img !== '' && img.indexOf('http') === -1){
+								 	icon = validateMedia(img) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'img : ' + img + " " + icon + '<br/>';
+							 	}
+							}
 
-				 	var poster = $(data).find("page").eq(i).attr('poster');
-				 	if(typeof poster !== typeof undefined && poster !== 'null' && poster !== ''){
-				 		icon = validateMedia(poster) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-				 		msg += 'poster : ' + poster + " " + icon + '<br/>';
-				 	}
-
-				 	var img = $(data).find("page").eq(i).attr('img');
-				 	if(typeof img !== typeof undefined && img !== 'null' && img !== '' && img.indexOf('http') === -1){
-				 		icon = validateMedia(img) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-				 		msg += 'img : ' + img + " " + icon + '<br/>';
-
-				 		//if mp4 check for .srt file
-				 		var ext = img.split('.').pop();
-				 		if(ext === 'mp4'){
-				 			var srt = img.replace(/\.[^/.]+$/, ".srt");
-				 			if($.inArray(srt, mediaArr) !== -1){
-				 				icon = validateMedia(srt) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-								msg += 'srt : ' + srt + " " + icon + '<br/>';
-				 			}
-				 		}
-				 	}
-
-				 	var audio = $(data).find("page").eq(i).attr('audio');
-				 	if(typeof audio !== typeof undefined && audio !== 'null' && audio !== ''){
-				 		icon = validateMedia(audio) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-				 		msg += 'audio : ' + audio + " " + icon+ '<br/>';
-				 	}
-
-				 	var content = $(data).find("page").eq(i).find("content").first().text();
-				 	var contentImgLength = $($.parseHTML(content)).find("img").length;
-				 	if(contentImgLength != 0){
-				 		for (var m = 0; m < contentImgLength; m++) {
-						 	var contentImg = $($.parseHTML(content)).find("img").eq(m).attr("src");
-						 	if(typeof contentImg !== typeof undefined && contentImg !== 'null' && contentImg !== '' && contentImg.indexOf('http') === -1){
-							 	icon = validateMedia(contentImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-							 	msg += 'content img : ' + contentImg + " " + icon + '<br/>';
+						 	var branchContent = $(data).find("page").eq(i).children("branch").eq(h).find("content").first().text();
+						 	var branchContentImgLength = $($.parseHTML(branchContent)).find("img").length;
+						 	if(branchContentImgLength != 0){
+						 		for (var m = 0; m < branchContentImgLength; m++) {
+								 	var branchContentImg = $($.parseHTML(branchContent)).find("img").eq(m).attr("src");
+								 	if(typeof branchContentImg !== typeof undefined && branchContentImg !== 'null' && branchContentImg !== '' && branchContentImg.indexOf('http') === -1){
+									 	icon = validateMedia(branchContentImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+									 	msg += 'content img : ' + branchContentImg + " " + icon + '<br/>';
+								 	}
+						 		}
 						 	}
-				 		}
-				 	}
 
-				 	// #4968
-				 	var contentHrefLength = $($.parseHTML(content)).find("a").length;
-				 	if(contentHrefLength != 0){
-				 		for(var n=0; n < contentHrefLength; n++){
-				 			var contentHref = $($.parseHTML(content)).find("a").eq(n).attr("href");
-				 			if(typeof contentHref !== typeof undefined && contentHref !== 'null' && contentHref !== '' && 
-				 				contentHref.indexOf('http') === -1 && contentHref.indexOf('mailto:') === -1){
-							 	icon = validateMedia(contentHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-							 	msg += 'content href : ' + contentHref + " " + icon + '<br/>';		
-						 	}		 			
-				 		}
-				 	}
+						 	var branchContentHrefLength = $($.parseHTML(branchContent)).find("a").length;
+						 	if(branchContentHrefLength != 0){
+						 		for(var n=0; n < branchContentHrefLength; n++){
+						 			var branchContentHref = $($.parseHTML(branchContent)).find("a").eq(n).attr("href");
+						 			if(typeof branchContentHref !== typeof undefined && branchContentHref !== 'null' && branchContentHref !== '' && 
+						 				branchContentHref.indexOf('http') === -1 && branchContentHref.indexOf('mailto:') === -1){
+									 	icon = validateMedia(branchContentHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+									 	msg += 'content href : ' + branchContentHref + " " + icon + '<br/>';		
+								 	}		 			
+						 		}
+						 	}							 									
+							
+						 	var sidebar = $(data).find("page").eq(i).children("branch").eq(h).find("sidebar").first().text();
+						 	var sidebarImgLength = $($.parseHTML(sidebar)).find("img").length;
+						 	if(sidebarImgLength != 0){
+						 		for (var m = 0; m < sidebarImgLength; m++) {
+								 	var sidebarImg = $($.parseHTML(sidebar)).find("img").eq(m).attr("src");
+								 	if(typeof sidebarImg !== typeof undefined && sidebarImg !== 'null' && sidebarImg !== '' && sidebarImg.indexOf('http') === -1){
+									 	icon = validateMedia(sidebarImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+									 	msg += 'sidebar img : ' + sidebarImg + " " + icon + '<br/>';
+								 	}
+						 		}
+						 	}
 
-				 	/////#4998/////
-				 	var question = $(data).find("page").eq(i).find("question").first().text();
-				 	var questionImgLength = $($.parseHTML(question)).find("img").length;
-				 	if(questionImgLength != 0){
-				 		for (var o = 0; o < questionImgLength; o++) {
-				 			var questionImg = $($.parseHTML(question)).find("img").eq(o).attr("src");
-				 			if(typeof questionImg !== typeof undefined && questionImg !== 'null' && questionImg !== '' && questionImg.indexOf('http') === -1){	
-							 	icon = validateMedia(questionImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-							 	msg += 'question img : ' + questionImg + " " + icon + '<br/>';
-						 	}				 			
-				 		}
-				 	}
+						 	var sidebarHrefLength = $($.parseHTML(sidebar)).find("a").length;
+						 	if(sidebarHrefLength != 0){
+						 		for(var n=0; n < sidebarHrefLength; n++){
+						 			var sidebarHref = $($.parseHTML(sidebar)).find("a").eq(n).attr("href");
+						 			if(typeof sidebarHref !== typeof undefined && sidebarHref !== 'null' && sidebarHref !== '' && 
+						 				sidebarHref.indexOf('http') === -1 && sidebarHref.indexOf('mailto:') === -1){
+									 	icon = validateMedia(sidebarHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+									 	msg += 'sidebar href : ' + sidebarHref + " " + icon + '<br/>';		
+								 	}		 			
+						 		}
+						 	}	
 
-				 	var questionHrefLength = $($.parseHTML(question)).find("a").length;
-				 	if(questionHrefLength != 0){
-				 		for (var p = 0; p < questionHrefLength; p++) {
-				 			var questionHref = $($.parseHTML(question)).find("a").eq(p).attr("href");
-				 			if(typeof questionHref !== typeof undefined && questionHref !== 'null' && questionHref !== '' && 
-				 				questionHref.indexOf('http') === -1 && questionHref.indexOf('mailto:') === -1){	
-							 	icon = validateMedia(questionHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-							 	msg += 'question href : ' + questionHref+ " " + icon + '<br/>';	
-						 	}				 			
-				 		}
-				 	}
+						}
+					}
+					else{	
 
-				 	var answerLength = $(data).find("page").eq(i).find("answer").length;
-				 	if(answerLength != 0){
-				 		for (var q = 0; q < answerLength; q++) {
-				 			var answerImg = $(data).find("page").eq(i).find("answer").eq(q).attr('img');
-							if(typeof answerImg !== typeof undefined && answerImg !== 'null' && answerImg !== '' && answerImg.indexOf('http') === -1){	
-							 	icon = validateMedia(answerImg ) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-							 	msg += 'answer img : ' + answerImg + " " + icon + '<br/>';									
-							}			 			
-				 		}
+						var type = $(data).find("page").eq(i).attr('layout');
+					 	msg += '<h3>' + $(data).find("page").eq(i).find('title').first().text() + ' - ' + type + '</h3>';
 
-				 	}
-				 	/////#4998/////
+					 	var poster = $(data).find("page").eq(i).attr('poster');
+					 	if(typeof poster !== typeof undefined && poster !== 'null' && poster !== ''){
+					 		icon = validateMedia(poster) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+					 		msg += 'poster : ' + poster + " " + icon + '<br/>';
+					 	}
 
-				 	var revealLength = $(data).find("page").eq(i).find("reveal").length;
-				 	if (revealLength != 0){
-				 		for(var o=0; o < revealLength; o++){
-				 			var revealImg = $(data).find("page").eq(i).find("reveal").eq(o).attr("img");
-				 			if(typeof revealImg !== typeof undefined && revealImg !== 'null' && revealImg !== '' && revealImg.indexOf('http') === -1){	
-							 	icon = validateMedia(revealImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
-							 	msg += 'reveal image : ' + revealImg + " " + icon + '<br/>';						 	
-						 	}		
-				 		}
+					 	var img = $(data).find("page").eq(i).attr('img');
+					 	if(typeof img !== typeof undefined && img !== 'null' && img !== '' && img.indexOf('http') === -1){
+					 		icon = validateMedia(img) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+					 		msg += 'img : ' + img + " " + icon + '<br/>';
+
+					 		//if mp4 check for .srt file
+					 		var ext = img.split('.').pop();
+					 		if(ext === 'mp4'){
+					 			var srt = img.replace(/\.[^/.]+$/, ".srt");
+					 			if($.inArray(srt, mediaArr) !== -1){
+					 				icon = validateMedia(srt) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+									msg += 'srt : ' + srt + " " + icon + '<br/>';
+					 			}
+					 		}
+					 	}
+
+					 	var audio = $(data).find("page").eq(i).attr('audio');
+					 	if(typeof audio !== typeof undefined && audio !== 'null' && audio !== ''){
+					 		icon = validateMedia(audio) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+					 		msg += 'audio : ' + audio + " " + icon+ '<br/>';
+					 	}
+
+					 	var content = $(data).find("page").eq(i).find("content").first().text();
+					 	var contentImgLength = $($.parseHTML(content)).find("img").length;
+					 	if(contentImgLength != 0){
+					 		for (var m = 0; m < contentImgLength; m++) {
+							 	var contentImg = $($.parseHTML(content)).find("img").eq(m).attr("src");
+							 	if(typeof contentImg !== typeof undefined && contentImg !== 'null' && contentImg !== '' && contentImg.indexOf('http') === -1){
+								 	icon = validateMedia(contentImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'content img : ' + contentImg + " " + icon + '<br/>';
+							 	}
+					 		}
+					 	}
+
+					 	// #4968
+					 	var contentHrefLength = $($.parseHTML(content)).find("a").length;
+					 	if(contentHrefLength != 0){
+					 		for(var n=0; n < contentHrefLength; n++){
+					 			var contentHref = $($.parseHTML(content)).find("a").eq(n).attr("href");
+					 			if(typeof contentHref !== typeof undefined && contentHref !== 'null' && contentHref !== '' && 
+					 				contentHref.indexOf('http') === -1 && contentHref.indexOf('mailto:') === -1){
+								 	icon = validateMedia(contentHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'content href : ' + contentHref + " " + icon + '<br/>';		
+							 	}		 			
+					 		}
+					 	}
+
+					 	// sidebar #5035
+					 	var sidebar = $(data).find("page").eq(i).find("sidebar").first().text();
+					 	var sidebarImgLength = $($.parseHTML(sidebar)).find("img").length;
+					 	if(sidebarImgLength != 0){
+					 		for (var m = 0; m < sidebarImgLength; m++) {
+							 	var sidebarImg = $($.parseHTML(sidebar)).find("img").eq(m).attr("src");
+							 	if(typeof sidebarImg !== typeof undefined && sidebarImg !== 'null' && sidebarImg !== '' && sidebarImg.indexOf('http') === -1){
+								 	icon = validateMedia(sidebarImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'sidebar img : ' + sidebarImg + " " + icon + '<br/>';
+							 	}
+					 		}
+					 	}
+
+					 	var sidebarHrefLength = $($.parseHTML(sidebar)).find("a").length;
+					 	if(sidebarHrefLength != 0){
+					 		for(var n=0; n < sidebarHrefLength; n++){
+					 			var sidebarHref = $($.parseHTML(sidebar)).find("a").eq(n).attr("href");
+					 			if(typeof sidebarHref !== typeof undefined && sidebarHref !== 'null' && sidebarHref !== '' && 
+					 				sidebarHref.indexOf('http') === -1 && sidebarHref.indexOf('mailto:') === -1){
+								 	icon = validateMedia(sidebarHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'sidebar href : ' + sidebarHref + " " + icon + '<br/>';		
+							 	}		 			
+					 		}
+					 	}				 	
+
+					 	/////#4998/////
+					 	var question = $(data).find("page").eq(i).find("question").first().text();
+					 	var questionImgLength = $($.parseHTML(question)).find("img").length;
+					 	if(questionImgLength != 0){
+					 		for (var o = 0; o < questionImgLength; o++) {
+					 			var questionImg = $($.parseHTML(question)).find("img").eq(o).attr("src");
+					 			if(typeof questionImg !== typeof undefined && questionImg !== 'null' && questionImg !== '' && questionImg.indexOf('http') === -1){	
+								 	icon = validateMedia(questionImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'question img : ' + questionImg + " " + icon + '<br/>';
+							 	}				 			
+					 		}
+					 	}
+
+					 	var questionHrefLength = $($.parseHTML(question)).find("a").length;
+					 	if(questionHrefLength != 0){
+					 		for (var p = 0; p < questionHrefLength; p++) {
+					 			var questionHref = $($.parseHTML(question)).find("a").eq(p).attr("href");
+					 			if(typeof questionHref !== typeof undefined && questionHref !== 'null' && questionHref !== '' && 
+					 				questionHref.indexOf('http') === -1 && questionHref.indexOf('mailto:') === -1){	
+								 	icon = validateMedia(questionHref) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'question href : ' + questionHref+ " " + icon + '<br/>';	
+							 	}				 			
+					 		}
+					 	}
+
+					 	var answerLength = $(data).find("page").eq(i).find("answer").length;
+					 	if(answerLength != 0){
+					 		for (var q = 0; q < answerLength; q++) {
+					 			var answerImg = $(data).find("page").eq(i).find("answer").eq(q).attr('img');
+								if(typeof answerImg !== typeof undefined && answerImg !== 'null' && answerImg !== '' && answerImg.indexOf('http') === -1){	
+								 	icon = validateMedia(answerImg ) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'answer img : ' + answerImg + " " + icon + '<br/>';									
+								}			 			
+					 		}
+
+					 	}
+					 	/////#4998/////
+
+					 	var revealLength = $(data).find("page").eq(i).find("reveal").length;
+					 	if (revealLength != 0){
+					 		for(var o=0; o < revealLength; o++){
+					 			var revealImg = $(data).find("page").eq(i).find("reveal").eq(o).attr("img");
+					 			if(typeof revealImg !== typeof undefined && revealImg !== 'null' && revealImg !== '' && revealImg.indexOf('http') === -1){	
+								 	icon = validateMedia(revealImg) !== true ? '<img src="css/images/wrong.png"/>' : '<img src="css/images/correct.png"/>';
+								 	msg += 'reveal image : ' + revealImg + " " + icon + '<br/>';						 	
+							 	}		
+					 		}
+					 	}
 				 	}
 
 				 	msg += '<hr/>';
@@ -354,6 +453,9 @@ function C_MediaValidator(_myItem, _myParent) {
 		        //ADD menu to stage
 		        $("#stage").append(msg);
 
+		        //lesson overall status
+			 	var lessonPass = allGood !== true ? '<b>Lesson does not pass Media Validator</b> <img src="css/images/wrong.png"/>' : '<b>Lesson passes Media Validator<b/> <img src="css/images/correct.png"/>';
+			 	msg += $('#allGood').append(lessonPass);
 
 		        $("#dialog-mv").dialog({
 		            modal: true,
@@ -393,7 +495,7 @@ function C_MediaValidator(_myItem, _myParent) {
      	_media = _media.replace('media/', '');
      	if($.inArray(_media, mediaArr) !== -1){
      		mediaArr = $.grep(mediaArr, function(value){
-     			return value != _media;
+     		    return value != _media;
      		});
      		removedArr.push(_media);
      		return true;
@@ -403,7 +505,8 @@ function C_MediaValidator(_myItem, _myParent) {
 	     	if($.inArray(_media, removedArr) !== -1){
 	     		return true;
 	     	}
-	     	else{    		
+	     	else{   
+	     		allGood = false; 		
      			return false;
      		}
      	}
